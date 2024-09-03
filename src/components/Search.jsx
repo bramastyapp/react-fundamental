12ef4b3b5e892558e399bbeb30f80104b8812abc
bramastyapp <bramastyapp@gmail.com>
2024-09-03 18:55:16 +0700

    fix: search dengan button dan key enter

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,15 +2,24 @@ import { useState } from "react";
 
 function Search(props) {
   const [search, setSearch] = useState("");
-  const onSearchChange = (event) => {
-    setSearch(event.target.value);
-    props.onSearchChange(event.target.value);
+  const onSearchChange = () => {
+    props.onSearchChange(search);
+  };
+
+  const searchKeydown = (e) => {
+    if (e.key === "Enter") onSearchChange();
   };
 
   return (
     <>
       <div>
-        Cari Artikel : <input type="text" onChange={onSearchChange} />
+        Cari Artikel :
+        <input
+          type="text"
+          onChange={(e) => setSearch(e.target.value)}
+          onKeyDown={searchKeydown}
+        />
+        <button onClick={onSearchChange}>cari</button>
       </div>
       <small>
         Ditemukan {props.totalPosts} data dari pencarian {search}
