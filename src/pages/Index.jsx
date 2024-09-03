0aa5d68f184792656c7f607b2909bd28de2faf12
bramastyapp <bramastyapp@gmail.com>
2024-09-03 19:24:18 +0700

    feat: fetch API dengan useEffect

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,11 +1,13 @@
 import postsData from "../posts.json";
 import Article from "../components/Article";
 import Search from "../components/Search";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Home() {
   const [posts, setPosts] = useState(postsData);
   const [totalPosts, setTotalPosts] = useState(0);
+  const [externalPosts, setExternalPosts] = useState([]);
+
   const onSearchChange = (value) => {
     const filteredPosts = postsData.filter((item) =>
       item.title.includes(value)
@@ -13,6 +15,20 @@ function Home() {
     setPosts(filteredPosts);
     setTotalPosts(filteredPosts.length);
   };
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => response.json())
+      .then((json) => setExternalPosts(json));
+  }, []);
+
+  useEffect(() => {
+    console.log("ada useEffect dari posts");
+  }, [posts]);
+
+  useEffect(() => {
+    console.log("render");
+  });
+
   return (
     <>
       <h1>Simple Blog</h1>
@@ -21,6 +37,15 @@ function Home() {
         return (
           <>
             <Article {...props} key={index} />
+          </>
+        );
+      })}
+      <hr />
+      <h3>External Posts</h3>
+      {externalPosts.map((item, index) => {
+        return (
+          <>
+            <div key={index}>- {item.title}</div>
           </>
         );
       })}
