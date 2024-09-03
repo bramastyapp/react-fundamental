6bf795b929ee2f57916cd167618effa5716143d6
bramastyapp <bramastyapp@gmail.com>
2024-09-03 18:04:10 +0700

    learn: Conditional Rendering

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,9 +1,18 @@
+const ArticleStatus = ({ isNew }) => {
+  return isNew && <span> -- Baru</span>;
+};
+const NewArticle = () => {
+  return <span> -- New</span>;
+};
 function Article(props) {
   return (
     <>
       <h3>{props.title}</h3>
       <small>
         Date : {props.date}, Tags : {props.tags.join(", ")}
+        Article
+        <ArticleStatus isNew={props.isNew} />
+        {props.isNew && <NewArticle />}
       </small>
       <br />
     </>
